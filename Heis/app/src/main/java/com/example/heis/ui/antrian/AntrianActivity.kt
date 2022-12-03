7af12d4ab1aed5b26d3941a3e1d32bb3ad014e39
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-03 12:56:22 +0700

    add dependecies like cardview, recyclerview, and glide

---
+++ Heis/app/src/main/java/com/example/heis/ui/antrian/AntrianActivity.kt
@@ -0,0 +1,11 @@
+package com.example.heis.ui.antrian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AntrianActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_antrian)
+    }
+}
