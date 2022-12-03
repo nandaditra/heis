b6c5f18dc43a9706e540365678657a160774fc44
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-03 14:49:21 +0700

    add antrian packages and finish them

---
+++ Heis/app/src/main/java/com/example/heis/ui/antrian/ListAntrianAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.heis.ui.antrian
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.heis.R
+
+class ListAntrianAdapter(private val listAntrian: ArrayList<Antrian>) : RecyclerView.Adapter<ListAntrianAdapter.ListViewHolder>() {
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+         var tvName :  TextView = itemView.findViewById(R.id.tv_item_nama)
+         var tvNoAntrian : TextView = itemView.findViewById(R.id.tv_item_noantrian)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_antrian, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val antrian = listAntrian[position]
+
+        holder.tvName.text = antrian.nama
+        holder.tvNoAntrian.text = antrian.noAntrian
+    }
+
+    override fun getItemCount(): Int {
+        return listAntrian.size
+    }
+}
